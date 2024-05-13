261ad0d3c81c166a5ea5ac030733755915296e7c
Julito01 <julitod55@hotmail.com>
2024-05-13 19:51:17 -0300

    Remove JWT instantiation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LoginComponent } from './pages/login/login.component'
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { NavbarComponent } from './shared/components/navbar/navbar.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { DialogComponent } from './shared/components/dialog/dialog.component'
 import { MaterialModule } from './shared/material/material.module'
+import { HttpConfigInterceptor } from './shared/services/HttpConfigInterceptor'
 
 @NgModule({
   declarations: [
@@ -29,7 +30,9 @@ import { MaterialModule } from './shared/material/material.module'
     ReactiveFormsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
