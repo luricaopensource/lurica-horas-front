146fd62732fde5db04f2ed02e7c40b5e7fce48f3
Julito01 <julitod55@hotmail.com>
2024-06-18 14:30:21 -0300

    Create projects component, module, routes and view

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { IProject } from 'src/app/shared/models/projects/projects'
+import { ModalService } from 'src/app/shared/services/modal/modal.service'
+import { ProjectService } from 'src/app/shared/services/project/project.service'
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +10,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  public projects: IProject[] = []
+  public form: FormGroup = new FormGroup({})
+  public formSubmitted: boolean = false
 
+  constructor(
+    private projectService: ProjectService,
+    private formBuilder: FormBuilder,
+    private modalService: ModalService) {
+    this.buildForm()
+    this.getProjects()
+  }
+
+  private async getProjects(): Promise<void> {
+    const projects = await this.projectService.getProjects()
+
+    this.projects = projects
+  }
+
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      company: ['', [Validators.required]],
+      currency: ['', [Validators.required]]
+    })
+  }
+
+  private openModal(modalTemplate: TemplateRef<any>, options: { size: string, title: string }) {
+    this.modalService
+      .open(modalTemplate, options)
+      .subscribe()
+  }
+
+  public openNewProjectModal(modalTemplate: TemplateRef<any>): void {
+    this.openModal(modalTemplate, { size: 'lg', title: 'Crear proyecto' })
+    this.resetForm()
+  }
+
+  public async createProject(): Promise<void> {
+    if (this.form.invalid) {
+      this.formSubmitted = true
+      return
+    }
+
+    const project: IProject = this.form.value
+
+    const response = await this.projectService.createProject(project)
+    if (response.id) {
+      this.getProjects()
+      this.modalService.close()
+      this.resetForm()
+    }
+  }
+
+  public isInvalidInput(inputName: string): boolean {
+    const input = this.form.get(inputName)
+
+    if (!input) return false
+
+    return input.invalid && (input.dirty || input.touched || this.formSubmitted)
+  }
+
+  private resetForm(): void {
+    this.form.reset()
+    this.formSubmitted = false
+  }
 }
