a443c03d8c59b54523e86260f3d5630c24719e26
alexisrivero <alexisr15494@gmail.com>
2024-09-23 16:04:02 -0300

    fix report endpoint call to localhost, now calls API_URL

--- src/app/shared/services/reports/reports.service.ts
+++ src/app/shared/services/reports/reports.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { firstValueFrom } from 'rxjs'
+import { environment } from 'src/environments/environment'
 
 interface GetReportsBody {
   employeeId?: number
@@ -15,12 +16,13 @@ interface GetReportsBody {
   providedIn: 'root'
 })
 export class ReportsService {
+  private BASE_URL = environment.API_URL;
 
   constructor(private readonly http: HttpClient) { }
 
   async getPdf(body: GetReportsBody): Promise<Blob> {
     return firstValueFrom(
-      this.http.post('http://localhost:3000/reports/hours', body, { responseType: 'arraybuffer' })
+      this.http.post(`${this.BASE_URL}/reports/hours`, body, { responseType: 'arraybuffer' })
     ).then(response => new Blob([response], { type: 'application/pdf' }))
   }
 }
